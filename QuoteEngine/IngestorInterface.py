94ef400f5aaf38da762c632029275e7f49078a5d
Samuel Ocampo <obs.sam@gmail.com>
2021-10-23 21:58:58 -0600

    Add PDF ingestor

--- QuoteEngine/IngestorInterface.py
+++ QuoteEngine/IngestorInterface.py
@@ -1,3 +1,4 @@
+import os
 from typing import List
 from QuoteEngine import QuoteModel
 from abc import ABC
@@ -12,5 +13,10 @@ class IngestorInterface(ABC):
     def parse(cls, path: str) -> List[QuoteModel]:
         pass
 
-    def get_extension(self, path: str) -> bool:
-        return path.split(".")[-1]
+    def get_file_extension(self, path: str) -> str:
+        filename, file_extension = os.path.splitext(path)
+        return file_extension
+
+    def get_filename(self, path: str) -> str:
+        filename,file_extension = os.path.splitext(path)
+        return filename
--- QuoteEngine/PDFIngestor.py
+++ QuoteEngine/PDFIngestor.py
@@ -1,10 +1,15 @@
 import subprocess
+import os
 from QuoteEngine import IngestorInterface, QuoteModel, TextIngestor
+
 
 class PDFIngestor(IngestorInterface):
     ALLOWED_EXTENSIONS = ['.pdf']
 
     def parse(cls, path: str):
-        subprocess.run(["ls", "-l"])
-        return TextIngestor().parse()
-            
+        filename = cls.get_filename(path);
+        tmp_file = f'{filename}_tmp.txt'
+        subprocess.run(["pdftotext", "-raw", path, tmp_file])
+        quotes = TextIngestor().parse(tmp_file)
+        os.remove(tmp_file)
+        return quotes
